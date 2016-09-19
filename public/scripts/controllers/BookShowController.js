657c5a4d98e49917c8e025eed4d08a31b714e55d
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-18 17:02:36 -0700

    shows one book

--- public/scripts/controllers/BookShowController.js
+++ public/scripts/controllers/BookShowController.js
@@ -6,15 +6,16 @@ BookShowController.$inject = ['$http', '$routeParams', '$location'];
 function BookShowController ($http, $routeParams, $location) {
   console.log('book-show-controller start');
   var vm = this;
-  bookId = $routeParams.id;
+  vm.bookId = $routeParams.id;
   vm.book = {};
   $http({
     method: 'GET',
-    url: 'https://super-crud.herokuapp.com/books/' + bookId
+    url: 'https://super-crud.herokuapp.com/books/' + vm.bookId
   }).then(
     function indexCallback(response) {
       console.log('book-show get success', response);
       vm.book = response.data;
+      console.log('book-show-book', vm.book);
     },
     function errorCallback(response) {
       console.log('book-show get error', response);
